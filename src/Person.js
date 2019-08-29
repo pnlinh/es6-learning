cc553a9ebc4574dc91c763f6d1a1e9093518b449
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-08-29 21:07:10 +0700

    Init

---
+++ src/Person.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        return this.name + ' say hello.';
+    }
+}
+
+console.log(new Person('pnlinh').greet());
